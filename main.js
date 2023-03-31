1f698819aa91e2cb6e70342f17100cc3e3882641
darrenjon <darrenlin23@gmail.com>
2023-03-31 12:58:48 +0800

    import agora sdk

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+let APP_ID = "YOUR AGORA APP ID"
+
 let localStream;
 let remoteStream;
 let peerConnection;
+
+let uid = String(Math.floor(Math.random() * 10000));
+let token = null;
+let client;
 
 let servers = {
   iceServers: [
@@ -12,17 +18,61 @@ let servers = {
 };
 
 let init = async () => {
+  // Create client
+  client = await AgoraRTM.createInstance(APP_ID)
+  await client.login({ uid, token })
+
+  const channel = client.createChannel("main")
+  channel.join();
+
+  channel.on('MemberJoined', handlePeerJoined)
+  client.on('MessageFromPeer', handleMessageFromPeer)
+
   // Get local stream
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: true,
-  });
+    audio: false,
+  })
 
   // Display local stream on user-1 video element
-  document.getElementById("user-1").srcObject = localStream;
+  document.getElementById("user-1").srcObject = localStream
 }
 
-let createPeerConnection = async (sdpType) => {
+let handlePeerJoined = async (MemberId) => {
+  console.log('A new peer has joined this room:', MemberId)
+  createOffer(MemberId)  
+}
+
+let handleMessageFromPeer = async (message, MemberId) => {
+  message = JSON.parse(message.text)
+  console.log('Message from peer:', message.type)
+
+  if (message.type === 'offer') {
+    
+    if (!localStream) {
+      localStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      })
+      document.getElementById("user-1").srcObject = localStream
+    }
+    document.getElementById("offer-sdp").value = JSON.stringify(message.offer)
+    createAnswer(MemberId)
+  }
+
+  if (message.type === 'answer') {
+    document.getElementById("answer-sdp").value = JSON.stringify(message.answer)
+    addAnswer()
+  }
+
+  if (message.type === 'candidate') {
+    if (peerConnection) {
+      peerConnection.addIceCandidate(message.candidate)
+    }
+  }
+}
+
+let createPeerConnection = async (sdpType, MemberId) => {
   // Create peer connection
   peerConnection = new RTCPeerConnection( servers );
   // Create remote stream
@@ -43,24 +93,26 @@ let createPeerConnection = async (sdpType) => {
   peerConnection.onicecandidate = async (event) => {
     if (event.candidate) {
       // Display candidate on candidate textarea
-      document.getElementById(sdpType).value = JSON.stringify(peerConnection.localDescription);
+      document.getElementById(sdpType).value = JSON.stringify(peerConnection.localDescription)
+      client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'candidate', 'candidate': event.candidate }) }, MemberId)
     }
   }
 }
 
 
-let createOffer = async () => {
-  createPeerConnection("offer-sdp");
+let createOffer = async (MemberId) => {
+  createPeerConnection("offer-sdp", MemberId);
 
   let offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer);
 
   // Display offer on offer-sdp textarea
-  document.getElementById("offer-sdp").value = JSON.stringify(offer);
+  document.getElementById("offer-sdp").value = JSON.stringify(offer)
+  client.sendMessageToPeer({ text:JSON.stringify({'type': 'offer', 'offer': offer})}, MemberId)
 }
 
-let createAnswer = async () => {
-  createPeerConnection("answer-sdp");
+let createAnswer = async (MemberId) => {
+  createPeerConnection("answer-sdp", MemberId);
 
   let offer = document.getElementById("offer-sdp").value;
   if (!offer) return alert("Please retrieve offer from peer first");
@@ -73,6 +125,7 @@ let createAnswer = async () => {
 
   // Display answer on answer-sdp textarea
   document.getElementById("answer-sdp").value = JSON.stringify(answer);
+  client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'answer', 'answer': answer }) }, MemberId)
 }
 
 let addAnswer = async () => {
@@ -86,8 +139,3 @@ let addAnswer = async () => {
 }
 
 init();
-
-// Create offer button
-document.getElementById("create-offer").addEventListener("click", createOffer);
-document.getElementById("create-answer").addEventListener("click", createAnswer);
-document.getElementById("add-answer").addEventListener("click", addAnswer);
